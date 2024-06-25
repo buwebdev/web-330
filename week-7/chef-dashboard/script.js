63e964ce862b22b018a57531c467481a3ca0214e
Richard Krasso <rkrasso@hotmail.com>
2024-06-25 11:20:38 -0500

    feat(): replaced week 7's starter files.

---
+++ week-7/chef-dashboard/script.js
@@ -0,0 +1,33 @@
+/*
+  Pragmatic JavaScript
+  Chapter 3
+  Programming Assignment
+
+  Author:
+  Date:
+  Filename: chefs.js
+*/
+
+"use strict";
+
+// TODO: Define an array of chef objects
+let chefs = [
+  // Each chef object should have a name, specialty, weakness, and restaurantLocation
+];
+
+// TODO: Define a function to retrieve the first chef's information
+function retrieveChef1() {
+  // This function should return a promise that resolves with the chef's information after a delay
+}
+
+// TODO: Define a function to retrieve the second chef's information
+function retrieveChef2() {
+  // This function should return a promise that resolves with the chef's information after a delay
+}
+
+// TODO: Define a function to retrieve the third chef's information
+function retrieveChef3() {
+  // This function should return a promise that resolves with the chef's information after a delay
+}
+
+// TODO: Use Promise.allSettled to retrieve all chefs' information and update the webpage accordingly
